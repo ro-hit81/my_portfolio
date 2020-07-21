47b8acd38a846cfce2fecbd07255820bc6cbb92b
Rohit Khati <50332813+ro-hit81@users.noreply.github.com>
2020-07-21 21:17:28 +0545

    Installing vuetify to app.

---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-navigation-drawer
+        permanent
+        expand-on-hover
+        >
+        <v-list>
+            <v-list-item link>
+                <v-list-item-icon>mdi-folder</v-list-item-icon>
+                <v-list-item-content>
+                    About
+                </v-list-item-content>
+            </v-list-item>
+        </v-list>
+
+    </v-navigation-drawer>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,13 @@
 <template>
-    <h1>HOME</h1>
+    <v-container>
+        <h1>HOME</h1>
+    </v-container>
 </template>
+
+<script>
+
+export default {
+    components: {
+    }
+}
+</script>
