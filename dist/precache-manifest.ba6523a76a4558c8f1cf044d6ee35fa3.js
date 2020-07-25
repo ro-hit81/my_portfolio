27d2867b59a11c065f9cd7f61afa1d552543bf4c
Rohit Khati <50332813+ro-hit81@users.noreply.github.com>
2020-07-25 17:45:05 +0545

    Adding dist file for deploying.

---
+++ dist/precache-manifest.ba6523a76a4558c8f1cf044d6ee35fa3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f7ee69b193a481327ac",
+    "url": "/css/app.94b1ad95.css"
+  },
+  {
+    "revision": "342dec1fb7ff43e078c1",
+    "url": "/css/chunk-vendors.f8449a6e.css"
+  },
+  {
+    "revision": "6df1b9095709efca812de493c365c22c",
+    "url": "/img/navbg.6df1b909.jpg"
+  },
+  {
+    "revision": "5e78283f823583661b725304e6ecabaa",
+    "url": "/img/rohit.5e78283f.jpg"
+  },
+  {
+    "revision": "16725aeaf8338c2b4059961df93cd40d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f7ee69b193a481327ac",
+    "url": "/js/app.ca922354.js"
+  },
+  {
+    "revision": "342dec1fb7ff43e078c1",
+    "url": "/js/chunk-vendors.f62e4f37.js"
+  },
+  {
+    "revision": "1557ee10be6fce58d7f9aae62c507da0",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
